07367e3a9845fdc4149f8a6bb83f56d9375bef45
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-07 18:09:55 +0700

    add application linear equation resolver

--- src/main/java/LinearEquationResolver.java
+++ src/main/java/LinearEquationResolver.java
@@ -1,7 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class LinearEquationReslover {
+public class LinearEquationResolver {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double a, b, x;
